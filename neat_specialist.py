34df7e575a4ee65104c89bbcf922ec1ee52013c0
Leo Classon <leoclasson@feweb60168.feweb.vu.nl>
2022-09-13 13:29:48 +0200

    neat_specialist

--- neat_specialist.py
+++ neat_specialist.py
@@ -2,12 +2,16 @@ import sys
 
 sys.path.insert(0, 'evoman')
 from environment import Environment
-from demo_controller import Player_Controller
+from evoman_framework_Group10.demo_controller import player_controller
+from evoman_framework_Group10.neat_controller import NeatController
 
 # imports other libs
 import time
 import numpy as np
 import os
+
+import neat
+import multiprocessing
 
 headless = True  # this does not display visuals and makes the experiment faster
 if headless:
@@ -23,7 +27,7 @@ n_hidden_neurons = 10
 env = Environment(experiment_name=experiment_name,
                   enemies=[1],
                   playermode="ai",
-                  player_controller=Player_Controller(n_hidden_neurons),
+                  player_controller=NeatController(),
                   enemymode="static",
                   level=1,
                   speed="fastest")
@@ -33,3 +37,49 @@ env.state_to_log()  # checks environment state
 ini = time.time()  # sets time marker
 
 run_mode = 'train'  # train or test
+
+
+# _______________Implement NEAT - 1st attempt____________________________
+
+# runs simulation
+def simulation(env, x):
+    f, p, e, t = env.play(pcont=x)
+    return f
+
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        genome.fitness = 0
+        genome.fitness = simulation(env, genome)
+
+
+def run(config_path):
+    # Load the confih file (neat_config)
+    neat_config = os.path.dirname('evoman_framework_Group10.neat_config.txt')
+    config_path = os.path.join(neat_config, 'neat_config.txt')
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    pop = neat.Population(config)
+
+    # Ass stdout reporter to show porgress
+    pop.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+    pop.add_reporter(neat.Checkpointer(5))  # don't know what is that
+
+    # Run NEAT for 30 generations
+    winner = pop.run(eval_genomes, 30)
+
+    # Show final stats
+    print('\nBest genome:\n{!s}'.format(winner))
+
+
+# ________________________________________________________________________
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'neat_config.txt')
+    run(config_path)
