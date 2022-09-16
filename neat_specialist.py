58d5394e0b262f6c355f4293eef5240211d06324
Leo Classon <leoclasson@Leos-MacBook-Pro.local>
2022-09-16 15:26:00 +0200

    working model

--- neat_specialist.py
+++ neat_specialist.py
@@ -2,7 +2,6 @@ import sys
 
 sys.path.insert(0, 'evoman')
 from environment import Environment
-from demo_controller import player_controller
 from neat_controller import NeatController
 
 # imports other libs
@@ -11,35 +10,7 @@ import numpy as np
 import os
 
 import neat
-import multiprocessing
 
-headless = True  # this does not display visuals and makes the experiment faster
-if headless:
-    os.environ["SDL_VIDEODRIVER"] = "dummy"
-
-experiment_name = 'neat_demo'
-if not os.path.exists(experiment_name):
-    os.makedirs(experiment_name)
-
-n_hidden_neurons = 10
-
-# this sets up the environment in an individual evolution mode
-env = Environment(experiment_name=experiment_name,
-                  enemies=[1],
-                  playermode="ai",
-                  player_controller=NeatController(),
-                  enemymode="static",
-                  level=1,
-                  speed="fastest")
-
-env.state_to_log()  # checks environment state
-
-ini = time.time()  # sets time marker
-
-run_mode = 'train'  # train or test
-
-
-# _______________Implement NEAT - 1st attempt____________________________
 
 # runs simulation
 def simulation(env, x):
@@ -48,9 +19,30 @@ def simulation(env, x):
 
 
 def eval_genomes(genomes, config):
+    generation = []
+    global gen
     for genome_id, genome in genomes:
         genome.fitness = 0
         genome.fitness = simulation(env, genome)
+        generation.append(genome.fitness)
+
+    fitness_gens.append(np.mean(generation))
+    fitness_max.append(np.max(generation))
+    fitness_std.append(np.std(generation))
+
+    # saving experiment information
+    file_aux = open(f"{experiment_name}/EXP_{i + 1}" + "/results.txt", "a")
+    file_aux.write("\ngen best mean std")
+    file_aux.write(
+        f"\ngen-{gen}: "
+        + str((fitness_max[-1]))
+        + " "
+        + str((fitness_gens[-1]))
+        + " "
+        + str((fitness_std[-1]))
+    )
+    file_aux.close()
+    gen += 1
 
 
 def run(configuration_filepath):
@@ -72,45 +64,60 @@ def run(configuration_filepath):
     pop.add_reporter(neat.Checkpointer(5))  # don't know what is that
 
     # Run NEAT for 30 generations
-    winner = pop.run(eval_genomes, 30)
+    winner = pop.run(eval_genomes, 3)
 
     # Show final stats
     print('\nBest genome:\n{!s}'.format(winner))
+    file_aux = open(f"{experiment_name}/EXP_{i + 1}" + "/results.txt", "a")
+    file_aux.write("\nbest genome: ")
+    file_aux.write(f'{winner}')
+    file_aux.close()
 
 
 # ________________________________________________________________________
 
 
 if __name__ == '__main__':
+
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'neat_config.txt')
-    run(config_path)
 
+    headless = True
+    if headless:
+        os.environ["SDL_VIDEODRIVER"] = "dummy"
 
-    N_runs = 10
-
-    experiment_name = "Neat_enemies_78"
+    experiment_name = "Neat_implemetation_1"
     # create directory if it does not exist yet
     if not os.path.exists(experiment_name):
         os.makedirs(experiment_name)
 
-    local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'neat_config.txt')
-    #run(config_path)
-
-
     # initializes simulation in individual evolution mode, for single static enemy.
     env = Environment(
         experiment_name=experiment_name,
-        enemies=[7, 8],
+        enemies=[2],
         playermode="ai",
         player_controller=NeatController(),
         enemymode="static",
         level=2,
         speed="fastest",
-        multiplemode="yes",
+        multiplemode="no",
         randomini="yes",
     )
 
-    run(config_path)
     # default environment fitness is assumed for experiment
+    env.state_to_log()  # checks environment state
+
+    N_runs = 1
+
+    for i in range(N_runs):
+        if not os.path.exists(f"{experiment_name}/EXP_{i + 1}"):
+            os.makedirs(f"{experiment_name}/EXP_{i + 1}")
+        # global variables for saving mean and max each generation
+
+        n_hidden_neurons = 10
+        fitness_gens = []
+        fitness_max = []
+        fitness_std = []
+        gen = 0
+
+        run('neat_config.txt')
