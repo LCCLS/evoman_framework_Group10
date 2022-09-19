7a4e922631d5e9a3aff1090fc8fd60375650c79d
Leo Classon <leoclasson@Leos-MacBook-Pro.local>
2022-09-19 13:15:59 +0200

    plotting merged. helper functions to concat dfs

--- NEAT/neat_utils.py
+++ NEAT/neat_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 
 
@@ -6,6 +7,7 @@ def average_experiment_gens(root_dir):
     """
     reads the individual dataframes from each experimetnal repetition and saves a new file which contains their average
     """
+    #  CHECK IN THE NEAT_SPECIALIST.PY FILE FOR N_RUNS AND UPDATE THE NUMBER OF FILES TO READ
 
     exp1 = pd.read_csv(f"{root_dir}/EXP_1/results.txt", index_col=False)
     exp2 = pd.read_csv(f"{root_dir}/EXP_2/results.txt", index_col=False)
@@ -18,10 +20,35 @@ def average_experiment_gens(root_dir):
     # exp9 = pd.read_csv(f"{root_dir}/EXP_9/results.txt")
     # exp10 = pd.read_csv(f"{root_dir}/EXP_10/results.txt")
 
-    # total_exp = pd.concat([exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9, exp10]).groupby(level=0).mean()    total_exp = pd.concat([exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9, exp10]).groupby(level=0).mean()
+    #  FINAL EXPERIMENT CONCATENATION OF DFS
+    # total_exp = pd.concat([exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9, exp10]).groupby(level=0).mean()
+
     total_exp = pd.concat([exp1, exp2, exp3]).groupby(level=0).mean()
 
     if not os.path.exists(f"{root_dir}/EXP_MEAN"):
         os.makedirs(f"{root_dir}/EXP_MEAN")
 
     total_exp.to_csv(f"{root_dir}/EXP_MEAN/results.txt")
+
+
+def generation_plotting(dir_filepath, enemy):
+    """
+    plotting the fitness per generation for the best and the mean values per generation
+    """
+    file = dir_filepath + '/results.txt'
+    generations = pd.read_csv(file)
+
+    ax = plt.gca()
+    generations.plot(kind='line', x='gen', y='mean', ax=ax)
+    generations.plot(kind='line', x='gen', y='best', ax=ax)
+
+    plt.title(f"Generational Fitness for Enemy {enemy}")
+    plt.xlabel("Generations")
+    plt.ylabel("Fitness")
+
+    plt.savefig(dir_filepath + '/generation_fitness.png')
+    #  plt.show()
+
+
+#  filepath = "NEAT_ENEMY_2/EXP_1/results.txt"  # practice filepath
+#  generation_plotting(filepath)
