3ca4ef3061e6f5dde1719989830d1f63c33b5b90
Leo Classon <leoclasson@Leos-MacBook-Pro.local>
2022-09-19 13:53:06 +0200

    using plotly now, appending bug fixed

--- NEAT/neat_utils.py
+++ NEAT/neat_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 import os
 
 
@@ -23,17 +24,24 @@ def average_experiment_gens(root_dir):
     #  FINAL EXPERIMENT CONCATENATION OF DFS
     # total_exp = pd.concat([exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9, exp10]).groupby(level=0).mean()
 
-    total_exp = pd.concat([exp1, exp2, exp3]).groupby(level=0).mean()
+    total_mean_exp = pd.concat([exp1, exp2, exp3]).groupby(level=0).mean()
+    total_std_exp = pd.concat([exp1, exp2, exp3]).groupby(level=0).std()
 
     if not os.path.exists(f"{root_dir}/EXP_MEAN"):
         os.makedirs(f"{root_dir}/EXP_MEAN")
 
-    total_exp.to_csv(f"{root_dir}/EXP_MEAN/results.txt")
+    elif not os.path.exists(f"{root_dir}/EXP_STD"):
+        os.makedirs(f"{root_dir}/EXP_STD")
+
+    total_mean_exp.to_csv(f"{root_dir}/EXP_MEAN/results.txt")
+    total_std_exp.to_csv(f"{root_dir}/EXP_STD/results.txt")
 
 
 def generation_plotting(dir_filepath, enemy):
     """
     plotting the fitness per generation for the best and the mean values per generation
+
+    --> using matplotlib so not so pretty
     """
     file = dir_filepath + '/results.txt'
     generations = pd.read_csv(file)
@@ -50,5 +58,26 @@ def generation_plotting(dir_filepath, enemy):
     #  plt.show()
 
 
-#  filepath = "NEAT_ENEMY_2/EXP_1/results.txt"  # practice filepath
-#  generation_plotting(filepath)
+def pretty_generation_plotting(dir_filepath, enemy):
+    """
+    plotting the fitness per generation for the best and the mean values per generation
+
+    --> using plotly which is muc nicer and does not mess up the plots
+    """
+    file = dir_filepath + '/results.txt'
+    generations = pd.read_csv(file)
+
+    fig = px.line(generations, x="gen", y=generations.columns[1:3])
+    fig.update_layout(
+        title={'text': f"Generational Fitness for Enemy {enemy}"},
+        xaxis_title="Generation",
+        yaxis_title="Fitness",
+        legend_title="Metrics", )
+    fig.write_image(dir_filepath + '/generation_fitness.png')
+    # fig.show()
+
+
+enemies = [2, 7, 8]
+for i in enemies:
+    filepath = f"../NEAT_ENEMY_{i}/EXP_1"  # practice filepath
+    pretty_generation_plotting(filepath, i)
